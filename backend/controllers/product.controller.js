f7a560c3668649b2f2144c3c334c7099f76a34f8
AnilBASARAN <nlbsrn@gmail.com>
2024-12-01 04:46:10 +0300

    added admin route and privilages

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,5 +1,11 @@
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async(res,req)=>{
-
+ try{
+    const products = await Product.find({}); // find all products
+    res.json({products})
+ }catch(error){
+    console.log("Error in getAllProducts controller",error.message);
+    res.status(500).json({message:"Server error",error:error.message});
+ }
 }
