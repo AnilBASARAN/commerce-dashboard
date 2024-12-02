2ae13413201182fe8b614de87f125b544f883954
AnilBASARAN <nlbsrn@gmail.com>
2024-12-02 06:10:22 +0300

    adding cart routes and controller

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,3 +1,15 @@
 export const addToCart = async (req,res)=>{
     
 }
+
+export const removeAllFromCart = async(req,res)=>{
+
+}
+
+export const updateQuantity = async(req,res)=>{
+
+}
+
+export const getCartProducts = async(req,res)=>{
+    
+}
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"
 import productRoutes from "./routes/product.routes.js"
+import cartRoutes from "./routes/cart.routes.js"
 import { connectDB } from "./DB/connectionDB.js";
 import cookieParser from "cookie-parser";
+
 dotenv.config(); // At the top of your index.js or server.js
 const app = express();
 app.use(express.json()); // allows us to parse imcoming requests:req.body
-app.use(cookieParser());
+app.use(cookieParser()); // allows us to parse cookies=> req.cookies.refreshToken;
 const PORT = process.env.PORT || 3000 ;
 
 app.use("/api/auth",authRoutes)
 app.use("/api/products",productRoutes)
+app.use("/api/cart",cartRoutes)
 
 
 app.listen(PORT,()=>{
