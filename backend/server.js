977b2f008d1664a806783f62988907650ab06b46
AnilBASARAN <nlbsrn@gmail.com>
2024-12-02 07:28:21 +0300

    added a coupon controller model and routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"
 import productRoutes from "./routes/product.routes.js"
 import cartRoutes from "./routes/cart.routes.js"
+import couponRoutes from "./routes/coupon.routes.js"
 import { connectDB } from "./DB/connectionDB.js";
 import cookieParser from "cookie-parser";
 
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 3000 ;
 app.use("/api/auth",authRoutes)
 app.use("/api/products",productRoutes)
 app.use("/api/cart",cartRoutes)
+app.use("/api/coupon",couponRoutes)
 
 
 app.listen(PORT,()=>{
