12d6a2018bf6fd982253405b4eb2662a1828919f
AnilBASARAN <nlbsrn@gmail.com>
2024-12-06 00:07:15 +0300

    making the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './index.css'
 
 function App() {
  
-
+//am back
   return (
    <div className='flex items-center justify-center h-screen '>
     <h1 className='text-5xl text-green-500'>Hello</h1>
